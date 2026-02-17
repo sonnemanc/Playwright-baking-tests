b3e041f4a0d339e8ce48181b6dbafc59ceb38a67
Corban <corban@clics.com>
2026-02-17 12:52:01 -0800

    added index page

---
+++ pages/recipes/recipe-index-page.ts
@@ -0,0 +1,17 @@
+import { Page, Locator } from "@playwright/test";
+
+export class IndexPage {
+  readonly page: Page;
+  readonly header: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.header = page.locator("h1");
+  }
+
+  async goto() {
+    await this.page.goto("https://sallysbakingaddiction.com/recipe-index/", {
+      waitUntil: "domcontentloaded",
+    });
+  }
+}
